6ac97c82870b50d826291cea3e114b8b7240d002
CalebVaccaro <calebvbusiness@gmail.com>
2023-12-13 10:45:30 -0500

    add passthrough copy for main dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");   
   eleventyConfig.addPassthroughCopy("media");   
+  eleventyConfig.addPassthroughCopy("main");
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./projects/*.md");
   });
